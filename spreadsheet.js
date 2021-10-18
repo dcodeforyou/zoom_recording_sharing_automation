ccc01e1ca4c4a8c9115ae665ddf84978a8068f7b
Dushyant <dcodeforyou@gmail.com>
2021-10-19 01:58:14 +0530

    removed arrow functions for consistency

---
+++ spreadsheet.js
@@ -0,0 +1,16 @@
+// Get metadata about spreadsheet
+        // const sheetInfo = await googleSheetsInstance.spreadsheets.get({
+        //     auth,
+        //     spreadsheetId,
+        // });
+
+        // console.log(sheetInfo);
+
+        //Read from the spreadsheet
+        // const readData = await googleSheetsInstance.spreadsheets.values.get({
+        //     auth, //auth object
+        //     spreadsheetId, // spreadsheet id
+        //     range: "Sheet1!A:A", //range of cells to read from.
+        // })
+
+        // console.log(readData);
